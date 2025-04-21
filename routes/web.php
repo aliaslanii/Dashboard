a4ae4c3e773909039ce5e52e397ad37c9c682fde
Ali Aslani <118122531+aliaslanii@users.noreply.github.com>
2025-04-21 08:50:34 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,4 @@ Route::get('posts/search', [\App\Http\Controllers\V1\Post\PostController::class,
 Route::resource('posts', \App\Http\Controllers\V1\Post\PostController::class)->middleware('auth');
 
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__.'/auth.php';
